2ffa7b57b43fcf2cbecee15a4916a11bb22bcfcc
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2018-06-05 16:02:31 -0600

    refactored CSS to SCSS; adjusted layout of landing page; added new logo; still working on color palette

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 (function (){
   'use strict';
 
-  angular
-  .module('chortlePortal',[])
-  
-})()
+  // angular
+  // .module('chortlePortal',[])
+
+  var app = angular.module('chortlePortal', []);
+
+  app.controller('formController', function($scope, $document){
+
+    $scope.Clear = function(){
+      angular.forEach(angular.element($document[0].querySelectorAll('input[type=text], input[type=email], input[type=password]')), function(elem, index){
+        console.log(elem.value);
+        elem.value='';
+      });
+      $scope.myForm.$setPristine();
+    }
+  });
+
+})();
